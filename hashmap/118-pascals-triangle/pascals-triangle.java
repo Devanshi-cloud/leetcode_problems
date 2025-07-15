3c6f0e38ce7968381cb949122cbccbe3db8b3e0d
Devanshi Jaiswal <devanshishs@gmail.com>
2025-07-15 16:37:49 +0530

    Time: 1 ms (83.15%) | Memory: 42.3 MB (14.36%) - LeetSync

--- hashmap/118-pascals-triangle/pascals-triangle.java
+++ hashmap/118-pascals-triangle/pascals-triangle.java
@@ -1,19 +1,20 @@
 class Solution {
-    public List<List<Integer>> generate(int numRows) {
+    public List<List<Integer>> generate(int numsRows) {
         List<List<Integer>> res = new ArrayList<>();
-        if(numRows == 0){
+        if(numsRows == 0){
             return res;
         }
-        List<Integer> Fr = new ArrayList<>();
-        Fr.add(1);
-        res.add(Fr);
-        if(numRows == 1){
+        List<Integer> prev = new ArrayList<>();
+        prev.add(1);
+            res.add(prev);
+        if(numsRows == 1){
             return res;
         }
-        for(int i =2; i<=numRows; i++){
+        
+        for(int i = 2; i<=numsRows; i++){
             List<Integer> curr = new ArrayList<>();
             curr.add(1);
-            for(int j =1 ; j<i-1; j++){
+            for(int j = 1; j<i-1; j++){
                 curr.add(res.get(i-2).get(j-1) + res.get(i-2).get(j));
             }
             curr.add(1);
