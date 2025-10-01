83fc04b1de6bfa5e60a6f35b32582e14b93a689e
Devanshi Jaiswal <devanshishs@gmail.com>
2025-10-01 17:08:20 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (98.66%) - LeetSync

--- hashmap/154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
+++ hashmap/154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
@@ -4,13 +4,13 @@ class Solution {
         while (st < end) {
             int mid = st + (end - st) / 2;
             if (nums[mid] > nums[end]) {
-                // min is in right half
+                
                 st = mid + 1;
             } else if (nums[mid] < nums[end]) {
-                // min is in left half including mid
+                
                 end = mid;
             } else {
-                // nums[mid] == nums[end] -> cannot decide, shrink end safely
+                
                 end--;
             }
         }
