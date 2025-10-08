35ff25e903abbefff04eb0e0ed58ac691cd93d37
Devanshi Jaiswal <devanshishs@gmail.com>
2025-10-08 11:18:43 +0530

    Time: 3 ms (88.45%) | Memory: 44.4 MB (27.10%) - LeetSync

--- hashmap/496-next-greater-element-i/next-greater-element-i.java
+++ hashmap/496-next-greater-element-i/next-greater-element-i.java
@@ -1,38 +1,21 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> hash = new HashMap<Integer, Integer>();
-        Stack<Integer> st = new Stack<Integer>();
-
-        int n= nums2.length;
-
-        for(int i = n-1; i>=0; i--){
+       Map<Integer, Integer> hash = new HashMap<>();
+       Stack<Integer> st = new Stack<>();
+       for(int i = nums2.length-1; i>=0; i--){
             int num = nums2[i];
             while(!st.isEmpty() && st.peek() <= num){
                 st.pop();
             }
-
-            // if(!st.isEmpty() && st.peek>num){
-               
-            // }
-             hash.put(num, st.isEmpty() ? -1 : st.peek());
-
+            
+                hash.put(num, st.isEmpty() ? -1 : st.peek());
+            
             st.push(num);
-        }
-
-        int[] ans = new int[nums1.length];
-
-        for(int i=0; i<nums1.length; i++){
+       }
+       int[] ans = new int[nums1.length];
+       for(int i=0; i<nums1.length; i++){
             ans[i] = hash.get(nums1[i]);
-        }
-        return ans;
+       }
+       return ans;
     }
 }
-
-// initialize
-// traverse nums2
-// pop smaller numbers
-// empty condition
-// stack push
-
-// get ready as per nums2
-
